152e7c2597eda2b3c872c24b33617d3c977f0c90
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-02 20:47:14 -0500

    created Blocks class
    completed Blocks constructor

---
+++ Blocks.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class Blocks{
+  public ArrayList<ArrayList<String>> cell = new ArrayList<ArrayList<String>>();
+// cell is a single box in the sudoku puzzle
+// it contains a 2D array list of Strings which are the numbers in the puzzle
+  public Block(){
+    // this creates a 3X3 arrayList
+    // each row being a new arraylist
+    for(int i = 0; i < 3; i++){
+      cell.add(new ArrayList<String>());
+      for(int j = 0; j < 3; j ++){
+      cell.get(i).add(" _ ");}}}
+  public ArrayList<ArrayList<String>> getCell(){
+    // accessor method for grid class
+    return cell;
+  }
