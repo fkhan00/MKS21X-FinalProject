63a5d50781c1b8966ba24e93499da24986b609e4
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-02 20:55:12 -0500

    didn't add remove method before

--- Blocks.java
+++ Blocks.java
@@ -3,7 +3,7 @@ public class Blocks{
   public ArrayList<ArrayList<String>> cell = new ArrayList<ArrayList<String>>();
 // cell is a single box in the sudoku puzzle
 // it contains a 2D array list of Strings which are the numbers in the puzzle
-  public Block(){
+  public Blocks(){
     // this creates a 3X3 arrayList
     // each row being a new arraylist
     for(int i = 0; i < 3; i++){
@@ -20,17 +20,23 @@ public class Blocks{
         if(cell.get(i).contains(" " + num + " ")){
           return true;}}
     return false;}
-    public boolean legal(int num){
-      // makes sure user doesn't break the rules
-      // by putting a number outside of the range [1,9]
-      // or putting a number that the cell already contains
-      if(num > 9 || num <= 0){
-         return false;}
-      if(cell.contains(num)){
-        return false;}
-      return true;}
-    public void add(int num, int x, int y){
-      //checks to see if it is legal
-      // at arraylist y and index x, it sets that value to num
-      if(legal(num)){
-        cell.get(y).set(x, " " + num + " ");}}
+
+  public boolean legal(int num){
+    // makes sure user doesn't break the rules
+    // by putting a number outside of the range [1,9]
+    // or putting a number that the cell already contains
+    if(num > 9 || num <= 0){
+       return false;}
+    if(cell.contains(num)){
+      return false;}
+    return true;}
+
+  public void add(int num, int x, int y){
+    //checks to see if it is legal
+    // at arraylist y and index x, it sets that value to num
+    if(legal(num)){
+      cell.get(y).set(x, " " + num + " ");}}
+
+  public void remove(int x, int y){
+    // just sets the String at position (x,y) to default
+    cell.get(y).set(x, " _ ");}
