86d97e4d9bc367e1135078ff88f8d9a52cbda40c
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-04 17:45:56 -0500

    created contains class
    This checks if the number already appears in the same row or column

--- Grids.java
+++ Grids.java
@@ -1,16 +1,41 @@
 import java.util.ArrayList;
 import java.util.Random;
-public class Grid extends Blocks{
+public class Grid extends Block{
   public ArrayList<ArrayList<Block>> grid = new ArrayList<ArrayList<Block>>();
   public int size;
   // grid is an 2D ArrayList of Blocks
   // it is the sudoku puzzle
-  public Grids(){
+  public Grid(){
     // initialzies 3 ArrayList Block objects and adds them to Grid
     // then initializes 3 Block objects for each arraylist
     size = 0;
     for(int i = 0; i < 3; i++){
       grid.add(new ArrayList<Block>());
       for(int j = 0; j < 3; j++){
-        grid.get(i).add(new Blocks());}}}
-      }
+        grid.get(i).add(new Block());}}}
+
+  public boolean contains(int num,int indexA, int indexB, int x, int y){
+    //if your block contains the number return true
+    if(grid.get(indexA).get(indexB).contains(num)) {
+      return true;}
+    for(int i = 0; i < 3; i++) {
+      for(int k = 0; k < 3; k++){
+      // going through each block that has the number
+      if((i != indexA && k != indexB) && grid.get(i).get(k).contains(num)){
+        for(int j = 0; j < 3; j++){
+          if(i == indexA && grid.get(i).get(k).getCell().get(j).get(x).equals(" " + num + " ")){
+            return true;}
+          if(k == indexB && grid.get(i).get(k).getCell().get(y).get(j).equals(" " + num + " ")){
+            return true;}
+          }}}}
+            //if you got here the grid doesn't "contain" the number
+    return false;}
+
+    public boolean legal(int num, int indexA, int indexB, int x, int y){
+      // checks to see if your input is a legal move
+      if(num > 9 || num < 0 || contains(num, indexA, indexB, x, y)){
+        return false;}
+      return true;}
+
+
+}
