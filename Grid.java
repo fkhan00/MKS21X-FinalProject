cd84adb7c9a347866bfff15a3d240bed6f72041a
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-07 20:59:15 -0500

    debugged Grid.java contains method works

---
+++ Grid.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class Grid extends Block{
+  public ArrayList<ArrayList<Block>> grid = new ArrayList<ArrayList<Block>>();
+  public int size;
+  // grid is an 2D ArrayList of Blocks
+  // it is the sudoku puzzle
+  public Grid(){
+    // initialzies 3 ArrayList Block objects and adds them to Grid
+    // then initializes 3 Block objects for each arraylist
+    size = 0;
+    for(int i = 0; i < 3; i++){
+      grid.add(new ArrayList<Block>());
+      for(int j = 0; j < 3; j++){
+        grid.get(i).add(new Block());}}}
+
+  public boolean containsRow(int num, int indexA, int indexB, int x, int y){
+    for(int i = 0; i < 3; i++) {
+      for(int k = 0; k < 3; k++){
+          if(grid.get(indexA).get(i).getCell().get(x).get(k).equals(" " + num + " ")){
+            return true;}}}
+    return false;
+  }
+
+  public boolean containsColumn(int num, int indexA, int indexB, int x, int y){
+    for(int i = 0; i < 3; i++) {
+      for(int k = 0; k < 3; k++){
+      // going through each block that has the number
+          if(grid.get(i).get(indexB).getCell().get(k).get(y).equals(" " + num + " ")){
+            return true;}}}
+    return false;}
+
+  public boolean contains(int num,int indexA, int indexB, int x, int y){
+    //if your block contains the number return true
+    if(grid.get(indexA).get(indexB).contains(num)) {
+      return true;}
+    return containsRow(num, indexA, indexB, x, y) || containsColumn(num, indexA, indexB, x, y);}
+
+    public boolean legal(int num, int indexA, int indexB, int x, int y){
+      // checks to see if your input is a legal move
+      if(num > 9 || num < 0 || contains(num, indexA, indexB, x, y)){
+        return false;}
+      return true;}
+
+    public boolean add(int num, int indexA, int indexB, int x, int y){
+      // calls on the super class' method of add to add to the block
+      if(legal(num, indexA, indexB, x, y)){
+        grid.get(indexA).get(indexB).add(num, x, y);
+        size++;
+        return true;
+      }
+      return false;
+    }
+    public void remove(int indexA, int indexB, int x, int y){
+      // calls on the super class' method of remove to the block
+      grid.get(indexA).get(indexB).remove(x, y);
+      size--;}
+    public String toString(){
+      // toString built from scratch
+      // 4 level nested for loop to make necessary adjustments to sudoku board
+  String output = "";
+  output += " ______________________________\n";
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      for(int k = 0; k < 3; k++){
+        for(int l = 0; l < 3; l++){
+          output += grid.get(i).get(k).getCell().get(j).get(l);
+        }
+        output += "|";
+      }
+      output += "\n";
+    }
+    output += "|\n ______________________________\n";
+  }
+  return output;
+}
+
+}
