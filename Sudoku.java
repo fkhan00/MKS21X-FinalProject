1170778c2ce489a3bb8be337cd7dd2feababd260
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-09 20:09:08 -0500

    fixed readPuzzle bug

--- Sudoku.java
+++ Sudoku.java
@@ -1,4 +1,3 @@
-//API : http://mabe02.github.io/lanterna/apidocs/2.1/
 import com.googlecode.lanterna.terminal.Terminal.SGR;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.input.Key;
@@ -16,6 +15,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Random;
 
 
 public class Sudoku extends Grid{
@@ -57,22 +57,58 @@ public class Sudoku extends Grid{
 			t.putCharacter(s.charAt(i));
 		}
 	}
+	public static Grid menu() throws FileNotFoundException{
+		Terminal terminal = TerminalFacade.createTextTerminal();
+		terminal.enterPrivateMode();
+		TerminalSize size = terminal.getTerminalSize();
+		terminal.setCursorVisible(false);
+		Grid puzzle = new Grid();
+		boolean running = true;
+		while(running == true){
+		putString(1,0, terminal, "WELCOME TO PSUEDOSUDOKHAN");
+		putString(1, 3, terminal, "Choose your difficulty");
+		putString(1,4, terminal, "E for easy, M for Medium, H, for Hard");
+		Key key = terminal.readInput();
+		if(key != null){
+		if (key.getKind() == Key.Kind.Escape) {
+			terminal.exitPrivateMode();
+			running = false;}
+		if (key.getCharacter() == 'E') {
+			terminal.clearScreen();
+			running = false;
+			Random randgen = new Random();
+			puzzle = readPuzzle("sE" + (Math.abs(randgen.nextInt() % 3)+ 1) + ".txt");}
+		if(key.getCharacter() == 'M'){
+			running = false;
+			terminal.clearScreen();
+			Random randgen = new Random();
+			puzzle = readPuzzle("sM" + (Math.abs(randgen.nextInt() % 3) + 1) + ".txt");}
+		if(key.getCharacter() == 'H'){
+			running = false;
+			terminal.clearScreen();
+			Random randgen = new Random();
+			puzzle = readPuzzle("sH" + (Math.abs(randgen.nextInt() % 3) + 1) + ".txt");}}}
+			return puzzle;
+	}
 	public static Grid readPuzzle(String file) throws FileNotFoundException{
-		// converts info given in file to Sudoku puzzle
-		File input = new File(file);
-		Grid output = new Grid();
-		int counter = 0;
-		Scanner parse = new Scanner(input);
-		while(parse.hasNextLine()){
-			for(int i = 0; i < parse.nextLine().length(); i++){
-				output.add(parse.nextInt(), counter / 3, i / 3, counter, i % 3);}
-			counter ++;}
-		return output;}
+    // converts info given in file to Sudoku puzzle
+    File input = new File(file);
+    Grid output = new Grid();
+    int counter = 0;
+    Scanner parse = new Scanner(input);
+    String text = parse.toString();
+    while(counter <= 9){
+      for(int i = 0; i < 18; i++){
+        try{
+        output.add(Integer.parseInt(text.substring(i, i + 1)), counter / 3, i / 3, counter, i % 3);}
+        catch(NumberFormatException e){}}
+      counter ++;}
+    return output;}
 
 	public static void main(String[] args) throws FileNotFoundException{
 		int old = 0;
 		//Grid puzzle = readPuzzle("" + args[0]);
-		Grid puzzle = new Grid();
+		Grid puzzle = menu();
 		int x = 1;
 		int y = 6;
 		int squareX = 0;
@@ -84,7 +120,7 @@ public class Sudoku extends Grid{
 		terminal.setCursorVisible(false);
 
 		boolean running = true;
-
+		menu();
 		long tStart = System.currentTimeMillis();
 		long lastSecond = 0;
 
@@ -206,11 +242,11 @@ public class Sudoku extends Grid{
 			putString(1,3, terminal, "(" + x + " , " + y + ")");
 			putString(1, 4, terminal, "(" + squareX + ", " + squareY +")");
 			putString(55, 0, terminal, "HOW TO PLAY");
-			putString(55, 1, terminal, "The goal of the game is to fill in the grid");
-			putString(55, 2, terminal, "in such a way that all of the rows, columns, and boxes");
-			putString(55, 3, terminal, "contains some arrangement of the sequence [1,9]");
-			putString(55, 4, terminal, "attempting to add a number that already appears in the same");
-			putString(55, 5, terminal, "row, column, or box will not work");
+			putString(55, 2, terminal, "The goal of the game is to fill in the grid");
+			putString(55, 4, terminal, "in such a way that all of the rows, columns, and boxes");
+			putString(55, 6, terminal, "contains some arrangement of the sequence [1,9]");
+			putString(55, 8, terminal, "attempting to add a number that already appears in the same");
+			putString(55, 10, terminal, "row, column, or box will not work");
 			if(millis/1000 > lastSecond){
 				lastSecond = millis / 1000;
 				//one second has passed.
