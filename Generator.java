3385bd2cc2bf35f4d65db47d0d0eb671eb4fbc01
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-14 17:39:39 -0500

    created working generator
    WARNING this generator has a try catch block command that will catch a Stack Overflow error and force the program to continue. This could freeze up your computer but I really wanted to see if it would work and it does took a long time but it did work out. There is clearly a lot of room for optimization and I think now that I have established the worst possible generator, it will only get better from here.

--- Generator.java
+++ Generator.java
@@ -1,41 +1,51 @@
 import java.util.Random;
 public class Generator extends Grid{
-  public static String generator(){
-  // simple Generator
-  // bashes numbers until it works
-  Random randgen = new Random();
-  int tries = 0;
-  Grid creation = new Grid();
-  int nSize = size;
-    while(! filled(creation)){
-    nSize = size;
-    creation.add(randgen.nextInt() % 10, Math.abs(randgen.nextInt() % 3), Math.abs(randgen.nextInt() % 3),
-    Math.abs(randgen.nextInt() % 3),Math.abs(randgen.nextInt() % 3));}
-    System.out.println(creation.toString());
-     if(! sum(creation)){
-      generator();}
-  return creation.toString();}
-  public static boolean filled(Grid creation){
-    for(int i = 0; i < 3; i++){
-      for(int j = 0; j < 3; j++){
-        for(int k = 0; k < 3; k++){
-          for(int l = 0; l < 3; l++){
-            if(creation.grid.get(i).get(j).getCell().get(k).get(l).equals(" _ ")){
-              return false;}}}}}
-    return true;
-  }
-  public static boolean sum(Grid creation){
-    int sum = 0;
-    for(int i = 0; i < 3; i++){
-      for(int j = 0; j < 3; j++){
-        for(int k = 0; k < 3; k++){
-          for(int l = 0; l < 3; l++){
-            sum += Integer.parseInt(creation.grid.get(i).get(j).getCell().get(k).get(l).substring(1,2));}}}}
-    if(sum != 45 * 9){
-      return false;}
-    return true;}
-    public static void main(String[] args){
-      System.out.println(generator());
-    }
+    public static String generator(){
+    // simple Generator
+    // bashes numbers until it works
+    Random randgen = new Random();
+    int tries = 0;
+    Grid creation = new Grid();
+    int nSize = creation.size;
 
-}
+      while(! filled(creation)){
+      nSize = creation.size;
+      tries = 0;
+      while(tries < 5000 && nSize == creation.size){
+      creation.add(randgen.nextInt() % 10, Math.abs(randgen.nextInt() % 3), Math.abs(randgen.nextInt() % 3),
+      Math.abs(randgen.nextInt() % 3),Math.abs(randgen.nextInt() % 3));
+      tries ++;}
+        }
+       if(! sum(creation)){
+        System.out.println(creation.toString());
+        return generator();}
+    return creation.toString();}
+
+    public static boolean filled(Grid creation){
+      for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+          for(int k = 0; k < 3; k++){
+            for(int l = 0; l < 3; l++){
+              if(creation.grid.get(i).get(j).getCell().get(k).get(l).equals(" _ ")){
+                return false;}}}}}
+      return true;
+    }
+    public static boolean sum(Grid creation){
+      int sum = 0;
+      for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+          for(int k = 0; k < 3; k++){
+            for(int l = 0; l < 3; l++){
+              try{
+              sum += Integer.parseInt(creation.grid.get(i).get(j).getCell().get(k).get(l).substring(1,2));}
+              catch(NumberFormatException e)
+              {return false;}
+            }}}}
+      if(sum != 45 * 9){
+        return false;}
+      return true;}
+      public static void main(String[] args){
+        System.out.println(generator());
+      }
+
+  }
