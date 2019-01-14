06f546241c30bd803575108068b7dc7e6ecce0be
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-01-14 18:45:42 -0500

    added the puzzle method
    This will produce a puzzle by removing numbers and then filling them in 50 times and seeing if each attempt gives the same grid. Basically it checks for uniqueness in a puzzle and removes random elements. The only major run time complexity is the hidden complexity of the generator function as the puzzle method calls that multiple times. So optimization of generator.java is completely reliant on the generator method.

--- Generator.java
+++ Generator.java
@@ -1,11 +1,10 @@
 import java.util.Random;
 public class Generator extends Grid{
-    public static String generator(){
+    public static Grid generator(Grid creation){
     // simple Generator
     // bashes numbers until it works
     Random randgen = new Random();
     int tries = 0;
-    Grid creation = new Grid();
     int nSize = creation.size;
 
       while(! filled(creation)){
@@ -18,8 +17,8 @@ public class Generator extends Grid{
         }
        if(! sum(creation)){
         System.out.println(creation.toString());
-        return generator();}
-    return creation.toString();}
+        return generator(new Grid());}
+    return creation;}
 
     public static boolean filled(Grid creation){
       for(int i = 0; i < 3; i++){
@@ -44,8 +43,38 @@ public class Generator extends Grid{
       if(sum != 45 * 9){
         return false;}
       return true;}
-      public static void main(String[] args){
-        System.out.println(generator());
-      }
 
+      public static Grid puzzle(Grid creation, int difficulty){
+        Random randgen = new Random();
+        String original = creation.toString();
+        int tries = 0;
+        int old = 0;
+        int squareX = 0;
+        int squareY = 0;
+        int x = 0;
+        int y = 0;
+        String edited = "";
+        while(creation.size != difficulty){
+          edited = creation.toString();
+          squareX = Math.abs(randgen.nextInt() % 3);
+          squareY = Math.abs(randgen.nextInt() % 3);
+          x = Math.abs(randgen.nextInt() % 3);
+          y = Math.abs(randgen.nextInt() % 3);
+          try{
+          old = Integer.parseInt(creation.grid.get(squareX).get(squareY).getCell().get(x).get(y));}
+          catch(NumberFormatException e){
+            old = 0;
+          }
+          creation.remove(squareX, squareY, x, y);
+          while(tries < 50){
+            if(!generator(creation).toString().equals(original)){
+              if(old != 0){
+              creation.add(old, squareX, squareY, x, y);}
+              break;}
+            tries ++;}}
+        return creation;
+      }
+      public static void main(String[] args){
+        System.out.println(puzzle(generator(new Grid()), 20).toString());
+      }
   }
